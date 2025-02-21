339e3ed4142d008486a62987cf855c3cbf8c126b
Huh Woosol <dnthf652@naver.com>
2025-02-22 04:29:28 +0900

    feat: 패키지 구성

---
+++ src/main/java/com/parking/admin/adapter/web/AdminController.java
@@ -0,0 +1,14 @@
+package com.parking.admin.adapter.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/admin")
+public class AdminController {
+
+}
